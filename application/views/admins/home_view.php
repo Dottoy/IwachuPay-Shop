ee4d9ba03104a51cd5d2e537d3101f3700e33027
Budida <bedabudida@gmail.com>
2017-06-29 12:30:47 +0300

    Fisrt commit all files

---
+++ application/views/admins/home_view.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>IwachuPay-Shop | Admin</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div id="container">
+	<div class='row'>
+	HEADER
+	</div>
+	<div class='row' id='contents-row' style='min-height:400px;'>
+		<div class='col-md-2 col-xs-3'>
+		<ul class="nav nav-pills nav-stacked">
+		  <li><a href="#">Home</a></li>
+		  <li><a href="#">Menu 1</a></li>
+		  <li><a href="#">Menu 2</a></li>
+		  <li><a href="#">Menu 3</a></li>
+		</ul>
+		</div>
+		<div class='col-md-10 col-xs-3'>
+		Right
+		</div>
+	</div>
+	<div class='row'>
+		FOOTER
+	</div>
+</div>
+
+</body>
+</html>
